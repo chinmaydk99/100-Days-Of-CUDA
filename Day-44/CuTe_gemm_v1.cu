092a499dcc5d20cca0125e385ec4df6526662652
chinmaydk99 <chinmay.dk@gmail.com>
2025-03-25 23:55:39 -0700

    Completed the kernel, main function pending

--- Day-44/CuTe_gemm_v1.cu
+++ Day-44/CuTe_gemm_v1.cu
@@ -36,9 +36,9 @@ gemm_device(ProblemShape shape_MNK,
     // k not specified since it will be looped over, we'll be needing all tiles
     
     // Global tensor reference, tile shape, which row and column specifically within the block, which modes to move along
-    Tensor gA = local_tile(mA, cta_tiler, cta_coord, Step<_1, X, _1>{}); // {BLK_M, BLK_K}, In step we are specifying that we will be moving along M and K modes
+    Tensor gA = local_tile(mA, cta_tiler, cta_coord, Step<_1, X, _1>{}); // {BLK_M, BLK_K, k}, In step we are specifying that we will be moving along M and K modes
     // This means local_tile(mA, select<0,2>(cta_tiler), select<0,2>(cta_coord))
-    Tensor gB = local_tile(mB, cta_tiler, cta_coord, Step<X, _1, _1>{}); // {BLK_K, BLK_N}
+    Tensor gB = local_tile(mB, cta_tiler, cta_coord, Step<X, _1, _1>{}); // {BLK_K, BLK_N, k}
     Tensor gC = local_tile(mC, cta_tiler, cta_coord, Step<_1, _1, X>{}); // {BLK_M, BLK_N}
 
     // Shared memory buffers
@@ -48,7 +48,60 @@ gemm_device(ProblemShape shape_MNK,
     Tensor sA = make_tensor(make_smem_ptr(smemA), sA_layout); // BLOCK_SIZE_M , BLOCK_SIZE_K
     Tensor sB = make_tensor(make_smem_ptr(smemB), sB_layout); // BLOCK_SIZE_N , BLOCK_SIZE_K
 
-    
+
+    // Copying tiles from global to shared memory 
+    // Naive way would be to use a single thread per block for this which while correct would be extremely slow
+    // Efficient way is to use threads to partition the copy
+    // We define a thread layout and partition the data so that each thread has its own view of a small chunk in both gA and sA
+    // In this case we already have access to the thread layouts as part of the inputs
+    Tensor tAgA = local_partition(gA, tA, threadIdx.x); // [THR_M, THR_K, k]
+    Tensor tAsA = local_partition(sA, tA, threadIdx.x); // [THR_M, THR_K]
+
+    Tensor tBgB = local_partition(gB, tB, threadIdx.x); // [THR_N, THR_K, k]
+    Tensor tBsB = local_partition(sB, tB, threadIdx.x); // [THR_N, THR_K]
+
+    CUTE_STATIC_ASSERT_V(size<0>(tAgA) == size<0>(tAsA)); // THR_M
+    CUTE_STATIC_ASSERT_V(size<1>(tAgA) == size<1>(tAsA)); // THR_K
+    CUTE_STATIC_ASSERT_V(size<0>(tBgB) == size<0>(tBsB)); // THR_N
+    CUTE_STATIC_ASSERT_V(size<1>(tBgB) == size<1>(tBsB)) ; // THR_K
+
+    // Local tile vs local partition: local tile does CTA level tiling while local partition does thread level partitioning
+    // For calculating the product again we define a thread layout to partition the workload
+
+    // Using thread layout tC to project threads onto rows of sA , columns of sB
+    // Partition sA by rows of tC. Only applying thread row indexing
+    Tensor tCsA = local_partition(sA, tC, threadIdx.x, Step<_1, X>{}); // (THR_M, BLOCK_SIZE_K), A rows for this thread
+    // Partition sB by cols of tC. Only applying thread column indexing
+    Tensor tCsB = local_partition(sB, tC, threadIdx.x, Step<X, _1>{}); // (THR_N, BLOCK_SIZE_K)
+    // Partition gC by tile of tC
+    Tensor tCgC = local_partition(gC, tC, threadIdx.x, Step<_1, _1>{}); //{THR_M, THR_N}
+
+    // Accumulator
+    Tensor tCrC = make_tensor_like(tCgC); // (THR_M, THR_N)
+
+    CUTE_STATIC_ASSERT_V(size<0>(tCrC) == size<0>(tCsA)); // THR_M
+    CUTE_STATIC_ASSERT_V(size<0>(tCrC) == size<0>(tCgC)); // THR_M
+    CUTE_STATIC_ASSERT_V(size<1>(tCrC) == size<1>(tCgC)); // THR_N                // THR_N
+    CUTE_STATIC_ASSERT_V(size<1>(tCrC) == size<0>(tCsB)); // THR_N               // THR_N
+    CUTE_STATIC_ASSERT_V(size<1>(tCsA) == size<1>(tCsB)); // BLOCK_K
+
+    // Main loop : Load-Compute Accumulate
+    auto K_TILE_MAX = size<2>(tAgA); // size<2>(tAgA) = k = Number of K tiles CTA will loop over
+
+    for(int k_tile = 0; k_tile < K_TILE_MAX; ++k_tile){
+        // Per thread copy from global to shared memory
+        copy(tAgA(_, _, k_tile), tAsA); // A (THR_M, THR_K) -> (THR_M, THR_K) 
+        copy(tBgB(_, _, k_tile), tBsB); // B (THR_N, THR_K) -> (THR_N, THR_K)
+
+        cp_async_fence();
+        cp_async_wait<0>();
+        __syncthreads();
+
+        gemm(tCsA, tCsB, tCrC); // (THR_M, THR_N) += (THR_M, BLK_K) * (THR_N, BLK_K)
+        __syncthreads();
+    }
+
+    axpby(alpha, tCrC, beta, tCgC); // C = alpha * Accumulator + beta * C
 }
 
 
@@ -86,7 +139,7 @@ void gemma_nt(int m, int n, int k,
     // This will be fed to a composition function to obtain tiles
 
     // Defining the shared memory layouts
-    // Static layouts arse more efficient and allow CuTe to have optimized implementations
+    // Static layouts are more efficient and allow CuTe to have optimized implementations
 
     auto sA = make_layout(make_shape(bM, bK)); // {BLOCK_SIZE_M, BLOCK_SIZE_K}, M-major
     auto sB = make_layout(make_shape(bN, bK)); // {BLOCK_SIZE_N, BLOCK_SIZE_K}, N-major
@@ -102,7 +155,7 @@ void gemma_nt(int m, int n, int k,
     dim3 dimGrid(size(ceil_div(M, bM)), 
                 size(ceil_div(N, bN)));
     
-    gemm_device<<<dimGrid, dimBlock 0, stream>>>(
+    gemm_device<<<dimGrid, dimBlock, 0, stream>>>(
         prob_shape, cta_tiler,
         A, dA, sA, tA,
         B, dB, sB, tB,
